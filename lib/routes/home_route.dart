7a900ab3934a0980ca64ea3c3deef0ffeb394a80
p32929 <p32929@yahoo.com>
2024-02-22 09:55:04 +0400

    numbers added

--- lib/routes/home_route.dart
+++ lib/routes/home_route.dart
@@ -7,6 +7,9 @@ class HomeRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double numberButtonsPadding = 12;
+    const double numberButtonsSize = 48;
+
     getInfoWidgets({String text = "0", IconData icon = Icons.menu}) {
       const dividerText = '-:-';
       return Expanded(
@@ -25,6 +28,21 @@ class HomeRoute extends StatelessWidget {
               ),
               Text(dividerText),
             ],
+          ),
+        ),
+      );
+    }
+
+    getNumberWidget() {
+      return ElevatedButton(
+        onPressed: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(numberButtonsSize),
+          child: Text(
+            "0",
+            style: TextStyle(
+              fontSize: numberButtonsSize,
+            ),
           ),
         ),
       );
@@ -59,6 +77,53 @@ class HomeRoute extends StatelessWidget {
                 getInfoWidgets(),
               ],
             ),
+            Padding(padding: const EdgeInsets.all(16)),
+            Padding(
+              padding: const EdgeInsets.all(numberButtonsPadding),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  getNumberWidget(),
+                  Padding(padding: const EdgeInsets.all(numberButtonsPadding)),
+                  getNumberWidget(),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(numberButtonsPadding),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  getNumberWidget(),
+                  Padding(padding: const EdgeInsets.all(numberButtonsPadding)),
+                  getNumberWidget(),
+                ],
+              ),
+            ),
+            Padding(padding: const EdgeInsets.all(16)),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 36.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        //
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          "START",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
