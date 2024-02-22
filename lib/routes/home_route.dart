182dcb6cdbe9d15dd693954d760b8e033d9cbb27
p32929 <p32929@yahoo.com>
2024-02-22 13:46:09 +0400

    okay start stop dynamic

--- lib/routes/home_route.dart
+++ lib/routes/home_route.dart
@@ -9,274 +9,275 @@ import 'package:google_fonts/google_fonts.dart';
 
 Timer? gameTimer;
 
+const double rowPaddings = 8;
+const double numberButtonSizePadding = 48;
+
 class HomeRoute extends StatelessWidget {
   const HomeRoute({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    const double rowPaddings = 8;
-    const double numberButtonSizePadding = 48;
-
-    getInfoWidgets(
-        {String text = "0", IconData icon = Icons.menu, Color? color}) {
-      const dividerText = '-:-';
-      return Expanded(
-        child: Padding(
-          padding: const EdgeInsets.all(rowPaddings),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                dividerText,
-                style: GoogleFonts.varelaRound(
-                  color: Colors.blueGrey,
-                ),
+  getInfoWidgets(
+      {String text = "0", IconData icon = Icons.menu, Color? color}) {
+    const dividerText = '-:-';
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(rowPaddings),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              dividerText,
+              style: GoogleFonts.varelaRound(
+                color: Colors.blueGrey,
               ),
-              Icon(
-                icon,
-                size: 32,
+            ),
+            Icon(
+              icon,
+              size: 32,
+              color: color,
+            ),
+            Text(
+              text,
+              style: GoogleFonts.varelaRound(
+                fontSize: 24,
                 color: color,
               ),
-              Text(
-                text,
-                style: GoogleFonts.varelaRound(
-                  fontSize: 24,
-                  color: color,
-                ),
-              ),
-              Text(
-                dividerText,
-                style: TextStyle(
-                  color: Colors.blueGrey,
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-
-    getResultButtonRippleColor(int index) {
-      if (index == states.state.correctAnsIndex) {
-        return Colors.green;
-      } else {
-        return Colors.red;
-      }
-    }
-
-    getNumberWidget({int index = 0}) {
-      return Expanded(
-        child: ElevatedButton(
-          style: ButtonStyle(
-            overlayColor: MaterialStateProperty.resolveWith(
-              (states) {
-                return states.contains(MaterialState.pressed)
-                    ? getResultButtonRippleColor(index)
-                    : null;
-              },
             ),
-          ),
-          onPressed: () {
-            if (states.state.correctAnsIndex == index) {
-              states.state.setTotalTrue(states.state.totalTrue + 1);
-            } else {
-              states.state.setTotalFalse(states.state.totalFalse + 1);
-            }
-          },
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: numberButtonSizePadding),
-            child: Text(
-              states.state.results[index].toString(),
-              style: GoogleFonts.varelaRound(
-                fontSize: numberButtonSizePadding,
+            Text(
+              dividerText,
+              style: TextStyle(
+                color: Colors.blueGrey,
               ),
             ),
-          ),
+          ],
         ),
-      );
-    }
+      ),
+    );
+  }
 
-    onSettingsClicked() {
-      OneContext.instance.showDialog(
-        builder: (p0) {
-          return StateBuilder(
-            observe: () => states,
-            builder: (context, _) {
-              return AlertDialog(
-                title: Text(
-                  "Settings",
-                  style: GoogleFonts.varelaRound(),
-                ),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Smallest number: ${states.state.minNumber}",
-                      style: GoogleFonts.varelaRound(
-                        fontSize: 16,
-                      ),
-                      textAlign: TextAlign.start,
-                    ),
-                    Slider(
-                      min: 1,
-                      max: states.state.maxNumber - 1,
-                      value: states.state.minNumber.toDouble(),
-                      onChanged: (value) {
-                        states.state.setMinNumber(value.toInt());
-                      },
-                    ),
-                    Padding(padding: const EdgeInsets.all(rowPaddings)),
-                    Text(
-                      "Biggest number: ${states.state.maxNumber}",
-                      style: GoogleFonts.varelaRound(
-                        fontSize: 16,
-                      ),
-                      textAlign: TextAlign.start,
-                    ),
-                    Slider(
-                      min: states.state.minNumber + 1,
-                      max: 99,
-                      value: states.state.maxNumber.toDouble(),
-                      onChanged: (value) {
-                        states.state.setMaxNumber(value.toInt());
-                      },
-                    ),
-                    Padding(padding: const EdgeInsets.all(rowPaddings)),
-                    Text(
-                      "Timer: ${states.state.maxTimer}",
-                      style: GoogleFonts.varelaRound(
-                        fontSize: 16,
-                      ),
-                      textAlign: TextAlign.start,
-                    ),
-                    Slider(
-                      min: 1,
-                      max: 999,
-                      value: states.state.maxTimer.toDouble(),
-                      onChanged: (value) {
-                        states.state.setTimer(value.toInt());
-                      },
-                    ),
-                  ],
-                ),
-                actions: [
-                  TextButton(
-                      onPressed: () {
-                        OneContext.instance.popAllDialogs();
-                      },
-                      child: Text(
-                        "Cancel",
-                        style: GoogleFonts.varelaRound(),
-                      )),
-                  TextButton(
-                      onPressed: () {
-                        OneContext.instance.popAllDialogs();
-                      },
-                      child: Text(
-                        "OK",
-                        style: GoogleFonts.varelaRound(),
-                      )),
-                ],
-              );
+  getResultButtonRippleColor(int index) {
+    if (index == states.state.correctAnsIndex) {
+      return Colors.green;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  getNumberWidget({int index = 0}) {
+    return Expanded(
+      child: ElevatedButton(
+        style: ButtonStyle(
+          overlayColor: MaterialStateProperty.resolveWith(
+            (states) {
+              return states.contains(MaterialState.pressed)
+                  ? getResultButtonRippleColor(index)
+                  : null;
             },
-          );
+          ),
+        ),
+        onPressed: () {
+          if (states.state.correctAnsIndex == index) {
+            states.state.setTotalTrue(states.state.totalTrue + 1);
+          } else {
+            states.state.setTotalFalse(states.state.totalFalse + 1);
+          }
+          setNewEquationAndResults();
         },
-      );
+        child: Padding(
+          padding:
+              const EdgeInsets.symmetric(vertical: numberButtonSizePadding),
+          child: Text(
+            states.state.results[index].toString(),
+            style: GoogleFonts.varelaRound(
+              fontSize: numberButtonSizePadding,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  onSettingsClicked() {
+    OneContext.instance.showDialog(
+      builder: (p0) {
+        return StateBuilder(
+          observe: () => states,
+          builder: (context, _) {
+            return AlertDialog(
+              title: Text(
+                "Settings",
+                style: GoogleFonts.varelaRound(),
+              ),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Smallest number: ${states.state.minNumber}",
+                    style: GoogleFonts.varelaRound(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  Slider(
+                    min: 1,
+                    max: states.state.maxNumber - 1,
+                    value: states.state.minNumber.toDouble(),
+                    onChanged: (value) {
+                      states.state.setMinNumber(value.toInt());
+                    },
+                  ),
+                  Padding(padding: const EdgeInsets.all(rowPaddings)),
+                  Text(
+                    "Biggest number: ${states.state.maxNumber}",
+                    style: GoogleFonts.varelaRound(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  Slider(
+                    min: states.state.minNumber + 1,
+                    max: 99,
+                    value: states.state.maxNumber.toDouble(),
+                    onChanged: (value) {
+                      states.state.setMaxNumber(value.toInt());
+                    },
+                  ),
+                  Padding(padding: const EdgeInsets.all(rowPaddings)),
+                  Text(
+                    "Timer: ${states.state.maxTimer}",
+                    style: GoogleFonts.varelaRound(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  Slider(
+                    min: 1,
+                    max: 999,
+                    value: states.state.maxTimer.toDouble(),
+                    onChanged: (value) {
+                      states.state.setTimer(value.toInt());
+                    },
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      OneContext.instance.popAllDialogs();
+                    },
+                    child: Text(
+                      "Cancel",
+                      style: GoogleFonts.varelaRound(),
+                    )),
+                TextButton(
+                    onPressed: () {
+                      OneContext.instance.popAllDialogs();
+                    },
+                    child: Text(
+                      "OK",
+                      style: GoogleFonts.varelaRound(),
+                    )),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  stopGame() {
+    states.state.setGameRunning(!states.state.isGameRunning);
+    states.state.setCurrentTimer(0);
+    gameTimer?.cancel();
+  }
+
+  int getMathResult(int num1, int num2, String operator) {
+    if (operator == '+') {
+      return num1 + num2;
+    } else if (operator == '-') {
+      return num1 - num2;
+    } else if (operator == 'X') {
+      return num1 * num2;
+    } else if (operator == '/') {
+      return num1 ~/ num2;
     }
 
-    stopGame() {
-      states.state.setGameRunning(!states.state.isGameRunning);
-      states.state.setCurrentTimer(0);
-      gameTimer?.cancel();
+    return 0;
+  }
+
+  setNewEquationAndResults() {
+    var numbersArray = Utils.generateNumberArray(
+      states.state.minNumber,
+      states.state.maxNumber,
+      shuffle: true,
+    );
+
+    int num1 = numbersArray[0];
+    int num2 = numbersArray[1];
+
+    if (num2 > num1) {
+      int temp = num1;
+      num1 = num2;
+      num2 = temp;
     }
 
-    int getMathResult(int num1, int num2, String operator) {
-      if (operator == '+') {
-        return num1 + num2;
-      } else if (operator == '-') {
-        return num1 - num2;
-      } else if (operator == 'X') {
-        return num1 * num2;
-      } else if (operator == '/') {
-        return num1 ~/ num2;
-      }
+    var operators = ["+", "-", "X", "/"];
+    var operatorsRand = Utils.generateNumberArray(
+      0,
+      operators.length - 1,
+      shuffle: true,
+    );
+    var op0 = operators[operatorsRand[0]];
+    states.state.setCurrentOperator(op0);
 
-      return 0;
+    if (op0 == "/") {
+      int mod = num1 % num2;
+      num1 -= mod;
+    }
+    states.state.setFirstNumber(num1);
+    states.state.setSecondNumber(num2);
+
+    List<int> results = [];
+    for (var i = 0; i < operators.length; i++) {
+      var op = operators[operatorsRand[i]];
+      var resultVal = getMathResult(num1, num2, op);
+      results.add(resultVal);
     }
 
-    setEquationAndResults() {
-      var numbersArray = Utils.generateNumberArray(
-        states.state.minNumber,
-        states.state.maxNumber,
-        shuffle: true,
-      );
-
-      int num1 = numbersArray[0];
-      int num2 = numbersArray[1];
-
-      if (num2 > num1) {
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
+    results = Utils.shuffleArray(results);
+    for (var i = 0; i < results.length; i++) {
+      var mr = getMathResult(num1, num2, op0);
+      if (results[i] == mr) {
+        states.state.setCorrectAnsIndex(i);
+        break;
       }
-
-      var operators = ["+", "-", "X", "/"];
-      var operatorsRand = Utils.generateNumberArray(
-        0,
-        operators.length - 1,
-        shuffle: true,
-      );
-      var op0 = operators[operatorsRand[0]];
-      states.state.setCurrentOperator(op0);
-
-      if (op0 == "/") {
-        int mod = num1 % num2;
-        num1 -= mod;
-      }
-      states.state.setFirstNumber(num1);
-      states.state.setSecondNumber(num2);
-
-      List<int> results = [];
-      for (var i = 0; i < operators.length; i++) {
-        var op = operators[operatorsRand[i]];
-        var resultVal = getMathResult(num1, num2, op);
-        results.add(resultVal);
-      }
-
-      results = Utils.shuffleArray(results);
-      for (var i = 0; i < results.length; i++) {
-        var mr = getMathResult(num1, num2, op0);
-        if (results[i] == mr) {
-          states.state.setCorrectAnsIndex(i);
-          break;
-        }
-      }
-      states.state.setResults(results);
     }
+    states.state.setResults(results);
+  }
 
-    startGame() {
-      states.state.setGameRunning(!states.state.isGameRunning);
-      setEquationAndResults();
-      gameTimer = Timer.periodic(Duration(seconds: 1), (timer) {
-        int timeLeft = states.state.maxTimer - timer.tick;
-        states.state.setCurrentTimer(timeLeft);
+  startGame() {
+    states.state.setGameRunning(!states.state.isGameRunning);
+    setNewEquationAndResults();
+    gameTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+      int timeLeft = states.state.maxTimer - timer.tick;
+      states.state.setCurrentTimer(timeLeft);
 
-        if (timeLeft <= 0) {
-          stopGame();
-        }
-      });
-    }
-
-    onStartStopClicked() {
-      if (states.state.isGameRunning) {
+      if (timeLeft <= 0) {
         stopGame();
-      } else {
-        startGame();
       }
-    }
+    });
+  }
 
+  onStartStopClicked() {
+    if (states.state.isGameRunning) {
+      stopGame();
+    } else {
+      startGame();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return StateBuilder(
       observe: () => states,
       builder: (context, _) => Scaffold(
