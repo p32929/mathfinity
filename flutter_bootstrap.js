35eee9e73154fc2ba3b2eaed9c0ae6635efdc6ee
p32929 <p32929@yahoo.com>
2024-12-10 03:03:42 +0400

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2379107691"
+    serviceWorkerVersion: "3531550345"
   }
 });
